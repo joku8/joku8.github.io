ddb699e2e4885dbb5d014537029de5d92f24ff22
Joseph Ku <josephku825@gmail.com>
2025-11-09 20:51:07 -0600

    config stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base: '/joku8.github.io/', // ← replace with your actual repo name
 })

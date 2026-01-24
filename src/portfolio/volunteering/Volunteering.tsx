d4c294470f5a4b1184ea075911425c63864f555a
Joseph Ku <josephku825@gmail.com>
2026-01-24 00:10:27 -0600

    remove backgorund cios logo

--- src/portfolio/volunteering/Volunteering.tsx
+++ src/portfolio/volunteering/Volunteering.tsx
@@ -1,19 +1,13 @@
-import { Image, SimpleGrid, Title } from '@mantine/core';
+import { Image, SimpleGrid, Title } from "@mantine/core";
 
-const imageFolder = '/images/volunteering'; // relative to /public
+const imageFolder = "/images/volunteering"; // relative to /public
 
 export const Volunteering = () => {
-
-  const imageList = [
-    'PCMG.png',
-    'GDMBG.jpg',
-    'GDMCB.jpg',
-    'CIOS.jpg',
-  ];
+  const imageList = ["PCMG.png", "GDMBG.jpg", "GDMCB.jpg", "CIOS.png"];
 
   return (
     <>
-      <Title order={2} style={{ marginBottom: '1rem' }}>
+      <Title order={2} style={{ marginBottom: "1rem" }}>
         Volunteering
       </Title>
       <SimpleGrid cols={4} spacing="md">
